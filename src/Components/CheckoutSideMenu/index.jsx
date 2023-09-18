ca4489d8aa94d30343f6bbbcb1fc9647f2d6195c
Daniel <danielcaz@gmail.com>
2023-09-17 21:35:59 -0300

    oncee

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,36 +1,41 @@
-// import "../../Components/ProductDetail/index.css"
-import { XCircleIcon } from '@heroicons/react/24/solid'
-import { useContext } from "react"
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
 import { OrderCard } from "../OrderCard";
-import "../CheckoutSideMenu/index.css"
+import "../CheckoutSideMenu/index.css";
 
-export const CheckoutSideMenu = ({ selecItem }) => {  
+export const CheckoutSideMenu = ({ selecItem }) => {
+  //console.log(selecItem   ) aca me traigo toda la imfomacion de la api q mapea el componente card
+  const Context = useContext(ShoppingCartContext); //traemos el Contexto(todas las funciones y variables), con esto le decimos que queremos q lea el estado global
 
-    
-    //console.log(selecItem   ) aca me traigo toda la imfomacion de la api q mapea el componente card
-    const Context = useContext(ShoppingCartContext)//traemos el Contexto(todas las funciones y variables), con esto le decimos que queremos q lea el estado global
-
-
-    return(
-        <aside className={`${Context.isCheckoutOpen ? 'fixed inset-0 flex checkout-side-menu flex-col right-0 border border-black rounded-lg bg-white' : 'hidden'}`}>
-            checau
-            <div className={`flex justify-between items-center p-6`}>
-                <div className='flex flex-col items-center'>
-                <h2 className='font-medium text-xl'>{selecItem  === null ?("dsa"): (`${"My orden"}`)}</h2>                 
-                </div>
-                <div className='cursor-pointer'onClick={() => Context.closeCheckout()}>                
-                <XCircleIcon className="h-6 w-6 text-black" />
-                </div>
-            </div>
-            <div className='px-6'>
-
-{
-            Context.dataCarritoProductos.map(product =>(
-                <OrderCard key={product.id} title={product.title} image={product.image} price={product.price}/>
-            ))
-}
-            </div>
-        </aside>
-    )
-}
+  return (
+    <aside
+      className={`${
+        Context.isCheckoutOpen
+          ? "fixed inset-0 flex checkout-side-menu flex-col right-0 border border-black rounded-lg bg-white"
+          : "hidden"
+      }`}
+    >
+      <div className={`flex justify-between items-center p-6`}>
+        <div className="flex flex-col items-center">
+          {/* <h2 className="font-medium text-xl">
+            {selecItem === null ? "dsa" : `${"My orden"}`}
+          </h2> */}
+        </div>
+        <div className="cursor-pointer" onClick={() => Context.closeCheckout()}>
+          <XCircleIcon className="h-6 w-6 text-black" />
+        </div>
+      </div>
+      <div className="px-6">
+        {Context.dataCarritoProductos.map((product) => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            image={product.image}
+            price={product.price}
+          />
+        ))}
+      </div>
+    </aside>
+  );
+};
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,4 +1,6 @@
 import { XCircleIcon } from '@heroicons/react/24/solid'
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context";
 
 export const OrderCard = props =>{
     // eslint-disable-next-line react/prop-types
@@ -9,6 +11,9 @@ export const OrderCard = props =>{
 //3.hacer q se bloquee el producto para agregar cuando ya este agregado con el id,
 //4.correr toda la ventana mas para la derecha
 //5.hacer el scroll para cuando agregue muchos productos.
+//6. closeCheckout cierra todo y lo q tiene q hacer es quitarlo de la array, creo q con un filter
+const Context = useContext(ShoppingCartContext)//traemos el Contexto(todas las funciones y variables), con esto le decimos que queremos q lea el estado global
+
 
     return(
 <div className='flex justify-between items-center mb-2'>
@@ -23,8 +28,10 @@ export const OrderCard = props =>{
     <div className='flex items-center gap-2'>
         <p className='text-lg font-medium'>
             {price}
-        </p>          
+        </p> 
+        <div className="cursor-pointer" onClick={() => Context.closeCheckout()}>
         <XCircleIcon className="h-6 w-6 text-black cursor-pointer" />        
+            </div>         
     </div>
 </div>
     )
