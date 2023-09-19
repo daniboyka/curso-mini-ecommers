9fd91e1fc685dc6b3ec55d0d2175eff3c98e4df0
Daniel <danielcaz@gmail.com>
2023-09-19 19:30:49 -0300

    trece

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -21,34 +21,49 @@ export const Card = ({ gatoi, item }) => { //TODO: ponerle un nombre mejor a la 
   Context.openProductDetail()  
 }
 
-const agregarProductoCarritoYCount = (event, a) =>{
+const agregarProductoCarritoYCount = (event, producto) =>{
   event.stopPropagation()
   Context.setCount(Context.count + 1)
-  Context.setDataCarritoProductos([...Context.dataCarritoProductos, a])//TODO: cambiar los argumentos "a" ///////   Context.dataCarritoProductos---> aca le decimos q muestre lo que ta tiene y con a--->va agregando los productos nuevos
+  Context.setDataCarritoProductos([...Context.dataCarritoProductos, producto])//Context.dataCarritoProductos---> aca le decimos q muestre lo que ta tiene y con producto--->va agregando los productos nuevos
   Context.closeProductDetail()
   Context.openCheckout()
 }
 
-const idsDeProductos = Context.dataCarritoProductos.map(producto => producto.id);
+const idsDeProductos = Context.dataCarritoProductos.map((producto) =>producto.id);
 //funcion que se fija si el producto ya esta en el carrito y cambia el icono de + a uno de check
 const check = (id) => {
-  // console.log(Context.dataCarritoProductos[0].id)
-  const meFijoSiYaestaenelcarro = Context.dataCarritoProductos.filter(producto => producto.id === id); 
+  if(Context.dataCarritoProductos){
+    return(
+      <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-9 h-9 rounded-full m-2 p-1"      
+      onClick={(event/*se le pasa un evento, para q solamente escuche el del cheacau por q sino el de detail lo tapa*/)=> agregarProductoCarritoYCount(event,item)}>    
+    <PlusCircleIcon className='h-16 w-16'/>
+    </div>
+    )
+  }
+ let productoEncontrado=null
 
-  if(meFijoSiYaestaenelcarro.length > 0) {
-    return(      
-      <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-9 h-9 rounded-full m-2 p-1">    
-          {/*TODO: fijarce como cambiar el problema de q si agregp algo al carrito me abre la pantalla detail */}
-          <CheckIcon className='h-16 w-16'/>
-          </div>
-          )
-        } else {
-          return(
-            <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-9 h-9 rounded-full m-2 p-1"      
-            onClick={(event/*se le pasa un evento, para q solamente escuche el del cheacau por q sino el de detail lo tapa*/)=> agregarProductoCarritoYCount(event,item)}>    
-          <PlusCircleIcon className='h-16 w-16'/>
-          </div>
-    )}
+  for (let i = 0; i < Context.dataCarritoProductos.length; i++) {
+    if (Context.dataCarritoProductos[i].id === id) {
+      // Si se encuentra el producto, asigna los datos a la variable productoEncontrado
+      productoEncontrado = Context.dataCarritoProductos[i];
+      break; // Termina el bucle una vez que se ha encontrado el producto
+       
+    }
+    if(productoEncontrado) {
+      return(      
+        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-9 h-9 rounded-full m-2 p-1">    
+            {/*TODO: fijarce como cambiar el problema de q si agregp algo al carrito me abre la pantalla detail */}
+            <CheckIcon className='h-16 w-16'/>
+            </div>
+            )
+          } else {
+            return(
+              <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-9 h-9 rounded-full m-2 p-1"      
+              onClick={(event/*se le pasa un evento, para q solamente escuche el del cheacau por q sino el de detail lo tapa*/)=> agregarProductoCarritoYCount(event,item)}>    
+            <PlusCircleIcon className='h-16 w-16'/>
+            </div>
+      )}
+  }
   
 }
 
