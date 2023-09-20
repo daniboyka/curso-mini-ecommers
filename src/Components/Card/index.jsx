941a0fcdc961d19bec9890e8be2eb03d9f278243
Daniel <danielcaz@gmail.com>
2023-09-20 18:36:34 -0300

    trece

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,85 +1,81 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
-import { PlusCircleIcon, CheckIcon } from '@heroicons/react/24/solid'
-import { useContext } from "react"
+import { PlusCircleIcon, CheckIcon } from "@heroicons/react/24/solid";
+import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
 
-
 // eslint-disable-next-line react/prop-types
-export const Card = ({ gatoi, item }) => { //TODO: ponerle un nombre mejor a la funcion gatoi
+export const Card = ({ gatoi, item }) => {
+  //TODO: ponerle un nombre mejor a la funcion gatoi
   // console.log("dsadsad",item)
-  const {category, title, image, price}=item
+  const { category, title, image, price, id } = item;
   
-  
-  const Context = useContext(ShoppingCartContext)//traemos el Contexto(todas las funciones y variables), con esto le decimos que queremos q lea el estado global
+
+  const Context = useContext(ShoppingCartContext); //traemos el Contexto(todas las funciones y variables), con esto le decimos que queremos q lea el estado global
   // const [datos, setDatos]= useState(item)
-  
-  
+
   //funcion q manda la informacion de los detalles a la ventanita
   const showProduct = () => {
-    gatoi()
-  Context.openProductDetail()  
-}
+    gatoi();
+    Context.openProductDetail();
+  };
 
-const agregarProductoCarritoYCount = (event, producto) =>{
-  event.stopPropagation()
-  Context.setCount(Context.count + 1)
-  Context.setDataCarritoProductos([...Context.dataCarritoProductos, producto])//Context.dataCarritoProductos---> aca le decimos q muestre lo que ta tiene y con producto--->va agregando los productos nuevos
-  Context.closeProductDetail()
-  Context.openCheckout()
-}
+  const agregarProductoCarritoYCount = (event, producto) => {
+    event.stopPropagation();
+    Context.setCount(Context.count + 1);
+    const probando = new Set([...Context.dataCarritoProductos, producto])
+    Context.setDataCarritoProductos([...probando])//Context.dataCarritoProductos---> aca le decimos q muestre lo que ta tiene y con producto--->va agregando los productos nuevos
+    Context.closeProductDetail();
+    Context.openCheckout();
+  };
 
-const idsDeProductos = Context.dataCarritoProductos.map((producto) =>producto.id);
-//funcion que se fija si el producto ya esta en el carrito y cambia el icono de + a uno de check
-const check = (id) => {
-  if(Context.dataCarritoProductos){
-    return(
-      <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-9 h-9 rounded-full m-2 p-1"      
-      onClick={(event/*se le pasa un evento, para q solamente escuche el del cheacau por q sino el de detail lo tapa*/)=> agregarProductoCarritoYCount(event,item)}>    
-    <PlusCircleIcon className='h-16 w-16'/>
-    </div>
-    )
-  }
- let productoEncontrado=null
+  //funcion que se fija si el producto ya esta en el carrito y cambia el icono de + a uno de check
 
-  for (let i = 0; i < Context.dataCarritoProductos.length; i++) {
-    if (Context.dataCarritoProductos[i].id === id) {
-      // Si se encuentra el producto, asigna los datos a la variable productoEncontrado
-      productoEncontrado = Context.dataCarritoProductos[i];
-      break; // Termina el bucle una vez que se ha encontrado el producto
-       
+  const idsDeProductos = Context.dataCarritoProductos
+console.log("luchooo", idsDeProductos)
+  // console.log("carrito", Context.dataCarritoProductos)
+
+  const check = (id) => {
+    // console.log("id", id)
+    const siEstaEnCard = Context.dataCarritoProductos.filter((producto) => producto.id === id).length > 0;
+    // console.log("filter", siEstaEnCard)
+
+    if (siEstaEnCard) {
+      return (
+        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-9 h-9 rounded-full m-2 p-1">
+          <CheckIcon className="h-16 w-16" />
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className="absolute top-0 right-0 flex justify-center items-center bg-white w-9 h-9 rounded-full m-2 p-1"
+          onClick={(event) => agregarProductoCarritoYCount(event, item)}>
+          <PlusCircleIcon className="h-16 w-16" />
+        </div>
+      );
     }
-    if(productoEncontrado) {
-      return(      
-        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-9 h-9 rounded-full m-2 p-1">    
-            {/*TODO: fijarce como cambiar el problema de q si agregp algo al carrito me abre la pantalla detail */}
-            <CheckIcon className='h-16 w-16'/>
-            </div>
-            )
-          } else {
-            return(
-              <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-9 h-9 rounded-full m-2 p-1"      
-              onClick={(event/*se le pasa un evento, para q solamente escuche el del cheacau por q sino el de detail lo tapa*/)=> agregarProductoCarritoYCount(event,item)}>    
-            <PlusCircleIcon className='h-16 w-16'/>
-            </div>
-      )}
-  }
-  
-}
+  };
 
-
-return (
-    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg"
-    onClick={() =>showProduct()}
+  return (
+    <div
+      className="bg-white cursor-pointer w-56 h-60 rounded-lg"
+      onClick={() => showProduct()}
     >
       <figure className="relative mb-2 w-full h-4/5">
-        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{category}</span>
-        <img className='w-full h-full object-cover rounded-lg' src={image} alt={title} />
-        {check(idsDeProductos)}
+        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
+          {category}
+        </span>
+        <img
+          className="w-full h-full object-cover rounded-lg"
+          src={image}
+          alt={title}
+        />
+        {check(id)}
       </figure>
-      <p className='flex justify-between'>
-        <span className='text-sm font-light truncate'>{title}</span>
-        <span className='text-lg font-medium mr-2'>${price}</span>
+      <p className="flex justify-between">
+        <span className="text-sm font-light truncate">{title}</span>
+        <span className="text-lg font-medium mr-2">${price}</span>
       </p>
     </div>
   );
