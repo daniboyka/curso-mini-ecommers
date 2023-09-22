de4e0cd37a463eeb721a5060a0d4fcdc005b5df5
Daniel <danielcaz@gmail.com>
2023-09-22 02:17:16 -0300

    quinto

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -11,14 +11,14 @@ export const PrecioTotal = () => {
 
   //aca sumo todo esos precios
   const valorTotalDeLosPrecios = PrecioTotalProducto.reduce((a, b) => a + b, 0);
-  setSumaDelPrecioTotal(valorTotalDeLosPrecios)
+  Context.setSumaDelPrecioTotal(valorTotalDeLosPrecios)
 
   //   console.log(sumaDelPrecioTotal)
 
   return (
     <div className="pl-6 w-full flex justify-between items-center">
         <p className="text-2xl font-medium">Total:</p>
-      <span className="text-2xl font-medium pr-6">{sumaDelPrecioTotal}</span>
+      <span className="text-2xl font-medium pr-6">{Context.sumaDelPrecioTotal}</span>
     </div>
   );
 };
