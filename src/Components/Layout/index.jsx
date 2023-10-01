6eb8be879bf247169d672cccb52c8e59e27b8a73
Daniel <danielcaz@gmail.com>
2023-09-30 21:31:03 -0300

    css

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 
 export const Layout = ({ children }) =>{
     return(
-        <div className='flex flex-col items-center mt-20'>
+        <div className='flex flex-col items-center mt-20 bg-gray-200'>
             {children}
         </div>
     )
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,84 +1,77 @@
-import { NavLink } from "react-router-dom"
-import { useContext } from "react"
-import { ShoppingCartContext } from "../../Context"
-import {  ShoppingBagIcon } from '@heroicons/react/24/outline'
-import { MySelectComponent } from "../NavSelect"
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
+import { ShoppingBagIcon } from "@heroicons/react/24/outline";
+import { MySelectComponent } from "../NavSelect";
 
 export const Navbar = () => {
-    const Context = useContext(ShoppingCartContext)//traemos el Contexto, con esto le decimos que queremos q lea el estado global
+  const Context = useContext(ShoppingCartContext); //traemos el Contexto, con esto le decimos que queremos q lea el estado global
 
-let EstaActivo = 'underline underline-offset-4'
+  let EstaActivo = "underline underline-offset-4";
 
-const handleCharacter = (evento) =>{
-    Context.setCaracter(evento.target.value)
-}
+  const handleCharacter = (evento) => {
+    Context.setCaracter(evento.target.value);
+  };
 
-
-    return(
-        <nav className='flex justify-between top-0 fixed z-10 w-full py-5 px-8 text-base font-normal'>
-            <ul className='flex items-center gap-3'>
-                <li className='font-semibold text-lg'>
-                    <NavLink to='/'>
-                        Shopi
-                    </NavLink>
-                </li>
-                <li>
-  <NavLink
-    to="/"
-    className={({ isActive }) =>
-      isActive ? EstaActivo : undefined
-    }
-    onClick={() => {
-      Context.setCaracter(null); // Restablece el filtro por caracteres
-      Context.setGetCaracterOpcion(""); // Restablece el filtro por categoría
-      Context.setFilteredItems(null); // Restablece los filtros
-   }}
-  >
-    All
-  </NavLink>
-</li>
-                <MySelectComponent/>
-            </ul>
-            <div>
-            <input
-            className='border border-black rounded-md w-72 p-1 focus:outline-none'
-            type='text'
-            placeholder='Buscar Producto...'
-            onChange={handleCharacter}
-            />
-            </div>
-            <ul className='flex item-center gap-3'>
-                <li className='text-black/60'>
-                    danielcaz@gmail.com
-                </li>
-                <li>
-                    <NavLink to='/my-orders'
-                    className={({ isActive }) =>
-                    isActive ? EstaActivo : undefined
-                  }>
-                        My Orders
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/my-account'
-                    className={({ isActive }) =>
-                    isActive ? EstaActivo : undefined
-                  }>
-                        My Account
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/sign-in'
-                    className={({ isActive }) =>
-                    isActive ? EstaActivo : undefined
-                  }>
-                        Sign In
-                    </NavLink>
-                </li>
-                <li className='flex items-center'>
-                    <ShoppingBagIcon className='h-6 w-6'/> {Context.count}
-                </li>               
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="flex justify-between item-center top-0 fixed z-10 mt-0 w-full py-5 px-8 text-base font-normal bg-orange-700">
+      <ul className="flex items-center gap-3 h-4 mt-0 mb-0">
+        <li className="font-semibold text-lg">
+          <NavLink to="/">Shopi</NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? EstaActivo : undefined)}
+            onClick={() => {
+              Context.setCaracter(null); // Restablece el filtro por caracteres
+              Context.setGetCaracterOpcion(""); // Restablece el filtro por categoría
+              Context.setFilteredItems(null); // Restablece los filtros
+            }}
+          >
+            All
+          </NavLink>
+        </li>
+        <MySelectComponent />
+      <div>
+        <input
+          className="border border-black rounded-md w-72 p-1 focus:outline-none"
+          type="text"
+          placeholder="Buscar Producto..."
+          onChange={handleCharacter}
+        />
+      </div>
+      </ul>
+      <ul className="flex items-center gap-3 h-4 mt-0 mb-0">
+        <li className="text-black/60">danielcaz@gmail.com</li>
+        <li>
+          <NavLink
+            to="/my-orders"
+            className={({ isActive }) => (isActive ? EstaActivo : undefined)}
+          >
+            My Orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/my-account"
+            className={({ isActive }) => (isActive ? EstaActivo : undefined)}
+          >
+            My Account
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/sign-in"
+            className={({ isActive }) => (isActive ? EstaActivo : undefined)}
+          >
+            Sign In
+          </NavLink>
+        </li>
+        <li className="flex items-center">
+          <ShoppingBagIcon className="h-6 w-6" /> {Context.count}
+        </li>
+      </ul>
+    </nav>
+  );
+};
